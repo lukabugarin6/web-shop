f4eb3fda6ecfb18a1e86e84dad1f5216ca10552d
s7lukabugarin <luka.bugarin@s7codedesign.com>
2021-03-02 17:02:08 +0100

    implement-add-to-cart

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,23 +1,31 @@
 import React from "react";
 
+import { useSelector } from 'react-redux';
+
 import { TiShoppingCart } from "react-icons/ti";
+import { Link } from "react-router-dom";
 import CustomContainer from "./CustomContainer";
 
 
 const Navbar = () => {
+  const cart = useSelector((state) => state.cart);
+  const { cartItems } = cart;
+
   return ( 
       <header className="text-quaternary">
         <CustomContainer className="flex justify-between flex-wrap items-center h-16">
-          <a href="/" className="text-3xl text-quaternary logo font-bold">
+          <Link to="/" className="text-3xl text-quaternary logo font-bold">
             Polovne<span className='text-quinary'>Knjige</span>
-          </a>
-          <div className="flex gap-x-8 text-xl ">
-            <a href="/cart" className="text-3xl hover:text-quinary">
-              <TiShoppingCart />
-            </a>
-            <a href="/sign-in" className="text-lg hover:text-quinary">
+          </Link>
+          <div className="flex gap-x-12 text-xl ">
+            <Link to="/cart" className="text-3xl hover:text-quinary">
+              <div className='relative'>
+              <TiShoppingCart /> {cartItems.length > 0 && <span className=' text-xs bg-quaternary py-0.5 px-1.5 rounded-full text-white absolute -right-4 top-2'>{cartItems.length}</span>}
+              </div>
+            </Link>
+            <Link to="/sign-in" className="text-lg hover:text-quinary">
               Sign in
-            </a>
+            </Link>
           </div>
         </CustomContainer>
       </header>
--- frontend/src/components/Product.js
+++ frontend/src/components/Product.js
@@ -1,11 +1,13 @@
 import React, { useState } from "react";
 
+import { Link } from 'react-router-dom';
+
 import Rating from "./Rating";
 
 const Product = ({ product }) => {
   return (
     <div className="col-span-2 text-primary">
-      <a href={`/product/${product._id}`}>
+      <Link to={`/product/${product._id}`}>
         <div className="h-56">
           <img
             className="h-full w-full object-cover"
@@ -13,12 +15,15 @@ const Product = ({ product }) => {
             alt={product.name}
           />
         </div>
+        </Link>
         <div className="py-4 text-center flex flex-col gap-y-2">
-          <h2 className="text-lg font-semibold">{product.name}</h2>
+            <Link to={`/product/${product._id}`}>  <h2 className="text-lg font-semibold">{product.name}</h2></Link>
+
+        
           <h3 className="text-sm font-semibold">{product.author}</h3>
           <p className="text-sm"> {product.price},00 RSD</p>
         </div>
-      </a>
+     
     </div>
   );
 };
