02650f4f1ae7bf4b2dbf60b437200b1732f97ae4
s7lukabugarin <luka.bugarin@s7codedesign.com>
2021-03-04 16:59:12 +0100

    user sign in case

--- frontend/src/reducers/userReducer.js
+++ frontend/src/reducers/userReducer.js
@@ -1,9 +1,14 @@
-import { USER_SIGNIN_REQUEST } from "../constants/userConstants";
+import {
+  USER_SIGNIN_REQUEST,
+  USER_SIGNIN_SUCCESS,
+} from "../constants/userConstants";
 
 export const userSigninReducer = (state = {}, action) => {
   switch (action.type) {
     case USER_SIGNIN_REQUEST:
       return { loading: true };
+    case USER_SIGNIN_SUCCESS:
+      return { loading: false, userInfo: action.payload };
     default:
       return state;
   }
